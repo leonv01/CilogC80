a5b956367bce28b186f99ba5f47f63c569056cb9
Leon <115944918+leonv01@users.noreply.github.com>
2024-11-21 23:15:14 +0100

    -Custom menubar

--- src/emulator/graphics_interface.c
+++ src/emulator/graphics_interface.c
@@ -3,31 +3,19 @@
 #include "raylib.h"
 
 #define RAYGUI_IMPLEMENTATION
-#define RAYGUI_CUSTOM_ICONS
-#include "gui_icons.h"
+//#define RAYGUI_CUSTOM_ICONS
+//#include "gui_icons.h"
 #include "raygui.h"
 
-#undef RAYGUI_IMPLEMENTATION            // Avoid including raygui implementation again
+#undef RAYGUI_IMPLEMENTATION         
 #define GUI_WINDOW_FILE_DIALOG_IMPLEMENTATION
 #include "gui_components/gui_file_dialog.h"
 
-#define GUI_WINDOW_PREFERENCES_IMPLEMENTATION
-#include "gui_components/gui_preferences.h"
+#define GUI_MENU_BAR_IMPLEMENTATION
+#include "gui_components/gui_menu_bar.h"
 
 #define GUI_TOOLTIPTTEXT_IMPLEMENTATION
 #include "gui_components/gui_tooltiptext.h"
-
-#define GUI_CPU_VIEW_IMPLEMENTATION
-#include "gui_components/gui_cpu_view.h"
-
-#define GUI_MENUBAR_IMPLEMENTATION
-#include "gui_components/gui_menubar.h"
-
-#define GUI_MEMORY_VIEW_IMPLEMENTATION
-#include "gui_components/gui_memory_view.h"
-
-#define GUI_FOOTER_BAR_IMPLEMENTATION
-#include "gui_components/gui_footer_bar.h"
 
 // Global variables
 // -----------------------------------------------------------
@@ -46,14 +34,6 @@ enum UiSize
     UI_SIZE_LARGE
 };
 
-typedef struct
-{
-    Vector2 position;
-    char *tooltip;
-    bool isActive;
-    int iconId;
-} GuiMenuButton;
-
 /* -------------------------------------------------------------------------- */
 /*                             Callback functions                             */
 /* -------------------------------------------------------------------------- */
@@ -67,141 +47,59 @@ int graphicsInit(int argc, char **argv, CPU_t *cpu, Memory_t *memory)
     
     int status;
 
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
+    int screenWidth = 1280;
+    int screenHeight = 720;
     
-    SetConfigFlags(FLAG_WINDOW_UNDECORATED | FLAG_VSYNC_HINT);
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_MINIMIZED);  // Set window configuration flags
     InitWindow(screenWidth, screenHeight, "Cilog C80 - Emulator");
 
-
-    GuiTooltipTextState tooltip = InitGuiToolTipText("Create new file", (Rectangle){ 0, 0, 150, 30 });
-
-    // Variables for file dialog
-    Texture texture = { 0 };
-    char fileNameToLoad[512] = { 0 };
+    /* -------------------------------- GUI Items ------------------------------- */
+    GuiMenuBarState menuBarState = InitGuiMenuBar((Vector2){ 0, 0 }, screenWidth, screenHeight * 0.05f);
     GuiWindowFileDialogState fileDialogState = InitGuiWindowFileDialog(GetWorkingDirectory());
-    GuiMenubarState menubarState = InitGuiMenubar();
-
-    GuiCpuViewState cpuViewState = InitGuiCpuView();
-
-    GuiMemoryViewState memoryViewState = InitGuiMemoryView();
-
-    GuiFooterBarState footerBarState = InitGuiFooterBar();
-
-    // Variables for the portable window
-    Vector2 mousePosition = { 0 };
-    Vector2 windowPosition = { 500, 200 };
-    Vector2 panOffset = mousePosition;
-    bool dragWindow = false;
-    bool exitWindow = false;
-    
-    SetWindowPosition(windowPosition.x, windowPosition.y);
-    
-    char ** guiIconsName = GuiLoadIcons("./resources/icons.rgi", true);
-    if(guiIconsName == NULL)
-    {
-        TraceLog(LOG_ERROR, "Failed to load icons");
-        return 1;
-    }
-
+    GuiTooltipTextState tooltipTextState = InitGuiToolTipText("Tooltip text", (Rectangle){ 0, 0, 100, 20 });
     //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!exitWindow && !WindowShouldClose())    // Detect window close button or ESC key
+    /* -------------------------------- Main loop ------------------------------- */
+    while (WindowShouldClose() == false)
     {
-        /* --------------------------------- Update --------------------------------- */
-        mousePosition = GetMousePosition();
-        
-        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && !dragWindow)
+        /* -------------------------- Handle window resize -------------------------- */
+        if(IsWindowResized())
         {
-            if (CheckCollisionPointRec(mousePosition, (Rectangle){ 0, 0, screenWidth, 20 }))
-            {
-                dragWindow = true;
-                panOffset = mousePosition;
-            }
+            screenWidth = GetScreenWidth();
+            screenHeight = GetScreenHeight();
+            GuiMenuBarResize(&menuBarState, screenWidth, screenHeight * 0.05f); //Todo: max and min height
+        }
+        /* -------------------------------------------------------------------------- */
+
+        /* ------------------------------ Logic updates ----------------------------- */
+        if(menuBarState.openButtonActive)
+        {
+            fileDialogState.windowActive = true;
         }
 
-        if (dragWindow)
-        {            
-            windowPosition.x += (mousePosition.x - panOffset.x);
-            windowPosition.y += (mousePosition.y - panOffset.y);
-
-            SetWindowPosition((int)windowPosition.x, (int)windowPosition.y);
-            
-            if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) dragWindow = false;
+        bool isAnyHovered = false;
+        char *toolTipText = GuiMenuBarGetTooltip(&menuBarState, &isAnyHovered);
+        if(isAnyHovered == false)
+        {
+            tooltipTextState.isActive = false;
         }
-        //----------------------------------------------------------------------------------
+        else
+        {
+            tooltipTextState.text = toolTipText;
+            tooltipTextState.isActive = true;
+        }
+        /* -------------------------------------------------------------------------- */
 
         /* ------------------------------ Begin Drawing ----------------------------- */
         BeginDrawing();
         {
-            ClearBackground(RAYWHITE);
+            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));  // Clear window background color
 
-            exitWindow = GuiWindowBox((Rectangle){ 0, 0, screenWidth, screenHeight }, "#198# Cilog C80");
-
-            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
-            //GuiSetStyle(DEFAULT, TEXT_SIZE, 20); // Change font size to 20
-            DrawTexture(
-                texture, 
-                GetScreenWidth() / 2 - texture.width / 2,
-                GetScreenHeight() / 2 - texture.height / 2 - 5,
-                WHITE);
-
-            DrawRectangleLines(
-                GetScreenWidth() / 2 - texture.width / 2,
-                GetScreenHeight() / 2 - texture.height / 2 - 5,
-                texture.width,
-                texture.height,
-                BLACK);
-
-            DrawText(fileNameToLoad, 208, GetScreenHeight() - 20, 10, GRAY);
-
-            if(fileDialogState.windowActive == true)
-            {
-                GuiLock();
-            }
-
-            /* --------------------------------- Menubar -------------------------------- */
-            if(menubarState.fileNewFilePressed == true)
-            {
-                fileDialogState.windowActive = true;
-            }
-            if(menubarState.fileOpenFilePressed == true)
-            {
-                fileDialogState.windowActive = true;
-            }
-            if(menubarState.fileSaveFilePressed == true)
-            {
-                fileDialogState.windowActive = true;
-            }
-            if(menubarState.fileCloseFilePressed == true)
-            {
-                fileDialogState.windowActive = false;
-            }
-            if(menubarState.viewCpuPressed == true)
-            {
-                cpuViewState.cpuAndRegisterWindowActive = !cpuViewState.cpuAndRegisterWindowActive;
-            }
-            if(menubarState.viewMemoryPressed == true)
-            {
-                memoryViewState.memoryWindowActive = !memoryViewState.memoryWindowActive;
-            }
-            /* -------------------------------------------------------------------------- */
-
-            GuiUnlock();
-
-            GuiMenubar(&menubarState);
-
-            GuiFooterBar(&footerBarState);
-
-            GuiCpuView(&cpuViewState);
-
-            GuiMemoryView(&memoryViewState);
-
+            /* -------------------------------- Draw GUI -------------------------------- */
+            GuiMenuBar(&menuBarState);
             GuiWindowFileDialog(&fileDialogState);
-
-            GuiToolTipText(&tooltip);
-
+            GuiToolTipText(&tooltipTextState);
+            /* -------------------------------------------------------------------------- */
         }
         EndDrawing();
         /* ------------------------------ End Drawing ----------------------------- */
