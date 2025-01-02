72170b5eaeafffc121d6b248c2ec7358eff44574
Leon <115944918+leonv01@users.noreply.github.com>
2025-01-02 01:55:56 +0100

    -Add graphics logic in graphics_unit

--- src/graphics_unit/graphics_unit.c
+++ src/graphics_unit/graphics_unit.c
@@ -9,3 +9,35 @@ void tms9918Init(TMS9918 *unit)
         unit->registers[idx] = 0x00;
     }
 }
+
+void tms9918Destroy(TMS9918 *unit)
+{
+    if(unit != NULL)
+    {
+        memoryDestroy(&(unit->vram));
+    }
+}
+
+void tms9918Reset(TMS9918 *unit)
+{
+    if(unit != NULL)
+    {
+        for(size_t idx = 0; idx < TMS_REGISTER_COUNT; idx++)
+        {
+            unit->registers[idx] = 0x00;
+        }
+
+        unit->vramAddress = 0x0000;
+        unit->mode = GRAPHICS_I_MODE;
+        
+        memoryReset(&(unit->vram));
+    }
+}
+
+void tms9918Step(TMS9918 *unit)
+{
+    if(unit != NULL)
+    {
+        
+    }
+}
--- src/graphics_unit/graphics_unit.h
+++ src/graphics_unit/graphics_unit.h
@@ -6,6 +6,9 @@
 
 #define TMS_REGISTER_COUNT 8
 
+#define TMS_SPRITE_COUNT 32
+
+
 typedef enum TMS9918_Mode
 {
     GRAPHICS_I_MODE = 0,
@@ -20,12 +23,27 @@ typedef struct TMS9918
     byte_t registers[TMS_REGISTER_COUNT];
     /* -------------------------------------------------------------------------- */
 
+    /** @brief VRAM memory */
     Memory_t vram;
+    /** @brief VRAM address (auto-incremented) */
+    word_t vramAddress;
 
     TMS9918_Mode mode;
 } TMS9918;
 
 void tms9918Init(TMS9918 *unit);
+void tms9918Destroy(TMS9918 *unit);
+void tms9918Reset(TMS9918 *unit);
+
+void tms9918Step(TMS9918 *unit);
+
+void tms9918WriteRegister(TMS9918 *unit, byte_t reg, byte_t value);
+byte_t tms9918ReadRegister(TMS9918 *unit, byte_t reg);
+
+void tms9918WriteVRAM(TMS9918 *unit, word_t address, byte_t value);
+byte_t tms9918ReadVRAM(TMS9918 *unit, word_t address);
+
+void tms9918SetMode(TMS9918 *unit, TMS9918_Mode mode);
 
 
 #endif //GRAPHICS_UNIT_H
